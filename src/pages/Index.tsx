77e4202c7bb54c79dfd7d634d36f059c4f068e9b
mxev9 <sasn1000@gmail.com>
2025-07-25 06:44:29 +0300

    First commit - prepare for Netlify

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import { WeeklySchedule } from "@/components/WeeklySchedule";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-gradient-dark">
+      <WeeklySchedule />
+    </div>
+  );
+};
+
+export default Index;
