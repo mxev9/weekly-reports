c8972a13771b9ba9435e028062126250fdacb689
mxev9 <sasn1000@gmail.com>
2025-07-28 09:22:42 +0300

    نشر gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/My-Weeks/',
+  base: '/weekly-reports/',
   plugins: [
     react(),
     mode === 'development' &&
